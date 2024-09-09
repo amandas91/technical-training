00fea7548fb58015323f5d88373db06db771d71e
Amanda Sabino <amandasabinoo1991@gmail.com>
2024-09-09 22:05:58 +0000

    add new sub menu

---
+++ estate/models.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+class TestModel(models.Model):
+    _name = "test_model"
+    _description = "Test Model"
+
+    name = fields.Char()
+    description = fields.Char()
