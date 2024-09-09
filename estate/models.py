6190f1cddf4c029875c80b0e0b7ae9870d4f7c84
Amanda Sabino <amandasabinoo1991@gmail.com>
2024-09-09 23:08:45 +0000

    Update model with attributes default

--- estate/models.py
+++ estate/models.py
@@ -1,5 +1,4 @@
 from odoo import fields, models
-
 class TestModel(models.Model):
     _name = "test_model"
     _description = "Test Model"
@@ -12,9 +11,9 @@ class TestModel(models.Model):
     facades = fields.Integer()
     garden = fields.Boolean()
     garden_area = fields.Float()
-    active = fields.Boolean()
+    active = fields.Boolean(default=False)
     selling_price = fields.Float(default=1000.00, readonly=True)
-    #avaliable_from = fields.Datetime("Last Seen", default=fields.Date.today())
+    avaliable_from = fields.Datetime("Avaliable", default=fields.Date.today())
     living_area = fields.Integer()
     garage = fields.Boolean()
     status = fields.Char()
