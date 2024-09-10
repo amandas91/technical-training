3db05cae9b4e935f6c25a5b6d3f1e1efe2b313b3
Amanda Sabino <amandasabinoo1991@gmail.com>
2024-09-10 23:14:43 +0000

    Add new field garden orientation

--- estate/models.py
+++ estate/models.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -13,11 +13,12 @@ class TestModel(models.Model):
     bedrooms = fields.Integer(default=2)
     facades = fields.Integer()
     garden = fields.Boolean()
-    garden_area = fields.Float()
+    garden_orientation = fields.Char()
+    garden_area = fields.Float(string="Garden area (sqm)")
     active = fields.Boolean(default=False)
     selling_price = fields.Float(default=1000.00, readonly=True, copy=False)
     available_from = fields.Datetime("Avaliable", default=fields.Date.today() + relativedelta(months=3), copy=False)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living area (sqm)")
     garage = fields.Boolean()
     status = fields.Char()
     
@@ -28,6 +29,18 @@ class TestModel(models.Model):
     tag_ids = fields.Many2many('real.estate.propety.tags', string="Property Tags")
 
     offer_ids = fields.Many2many('real.estate.propety.offer', string="Property Offers")
+
+    total_area = fields.Float(compute="_compute_total_area")
+    
+    @api.depends("living_area","garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area * record.living_area
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        self.garden = 10
+
 
 class RealEstatePropertyType (models.Model):
     _name = "real.estate.propety.type"
@@ -46,7 +59,10 @@ class RealEstatePropertyOffer (models.Model):
     _description = "The Real Estate Property Offer table"
 
     price = fields.Float(required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_sum_date_deadline")
 
+    
     def _get_estados(self):
         return [
             ('accepted', 'Accepted'),
@@ -62,5 +78,13 @@ class RealEstatePropertyOffer (models.Model):
     partner_id = fields.Many2one('res.partner', string="Partner")
     property_id = fields.Many2one('test.model', string="Estate Model")
     
+    @api.depends("validity")
+    def _sum_date_deadline(self):
+        for record in self:
+            record.date_deadline = record.create_date + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.date_deadline = record.date_deadline_from
 
     
